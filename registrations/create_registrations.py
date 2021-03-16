d97420933efc89390b3714545ed91fb0f29f01a8
Eirik Nilsen <eirik.nilsen@unit.no>
2021-03-16 08:25:34 +0100

    Fixed create registration after database changes

--- registrations/create_registrations.py
+++ registrations/create_registrations.py
@@ -1,64 +1,147 @@
 import boto3
 import json
 import uuid
+import copy
 
 dynamodb_client = boto3.client('dynamodb')
 
-registrations_tablename = 'nva_resources'
+RESOURCE_TABLE_NAME = 'nva_resources'
+CUSTOMERS_TABLENAME = 'nva_customers'
+ARCHIVE_NAME = 'API Test Archive'
+USER_NAME = 'api-test-user@test.no'
 
-def scan_registrations():
-    print('scanning registrations')
-    response = dynamodb_client.scan(TableName=registrations_tablename)
-    scanned_registrations = response['Items']
+
+def scan_items(tableName):
+    print('scanning items')
+    response = dynamodb_client.scan(TableName=tableName)
+    scanned_items = response['Items']
     more_items = 'LastEvaluatedKey' in response
     while more_items:
         start_key = response['LastEvaluatedKey']
-        response = dynamodb_client.scan(TableName=registrations_tablename, ExclusiveStartKey=start_key)
-        scanned_registrations.extend(response['Items'])
+        response = dynamodb_client.scan(
+            TableName=RESOURCE_TABLE_NAME, ExclusiveStartKey=start_key)
+        scanned_items.extend(response['Items'])
         more_items = 'LastEvaluatedKey' in response
-    return scanned_registrations
+    return scanned_items
+
 
 def delete_registrations():
     print('Deleting registrations...')
-    registrations = scan_registrations()
-    for registration in registrations:
-        modifiedDate = registration['modifiedDate']['S']
-        identifier = registration['identifier']['S']
-        owner = registration['owner']['S']
-        if('api-test-user@test.no') in owner:
-            print(
-                'Deleting {} - {}'.format(identifier, owner))
-            try:
-                response = dynamodb_client.delete_item(
-                    TableName=registrations_tablename,
-                    Key={
-                        'identifier': {
-                            'S': identifier
-                        },
-                        'modifiedDate': {
-                            'S': modifiedDate
-                        }
-                    })
-            except e:
-                print(e)
+    resources = scan_items(tableName=RESOURCE_TABLE_NAME)
+    for resource in resources:
+        if resource['type']['S'] == 'Resource':
+            primary_partition_key = resource['PK0']['S']
+            primary_sort_key = resource['SK0']['S']
+            registration = resource['data']['M']
+            identifier = registration['identifier']['S']
+            owner = registration['owner']['S']
+            if USER_NAME in owner:
+                print(
+                    'Deleting {} - {}'.format(identifier, owner))
+                try:
+                    response = dynamodb_client.delete_item(
+                        TableName=RESOURCE_TABLE_NAME,
+                        Key={
+                            'PK0': {
+                                'S': primary_partition_key
+                            },
+                            'SK0': {
+                                'S': primary_sort_key
+                            }
+                        })
+                except e:
+                    print(e)
+
+
+def find_customer(archiveName):
+    customers = scan_items(tableName=CUSTOMERS_TABLENAME)
+    for customer in customers:
+        if customer['archiveName']['S'] == archiveName:
+            return customer['identifier']['S']
+    return 'Not found'
+
+
+def create_pk0(pk0_template, customer, username):
+    pk0 = pk0_template.replace('<type>', 'Resource').replace(
+        '<customerId>', customer).replace('<userId>', username)
+    return pk0
+
+
+def create_pk1(pk1_template, customer, status):
+    pk1 = pk1_template.replace('<type>', 'Resource').replace(
+        '<customerId>', customer).replace('<status>', status)
+    return pk1
+
+
+def create_pk2(pk2_template, customer, identifier):
+    pk2 = pk2_template.replace('<type>', 'Resource').replace(
+        '<customerId>', customer).replace('<resourceId>', identifier)
+    return pk2
+
+
+def create_resource_key(template, identifier):
+    key = template.replace('<type>', 'Resource').replace(
+        '<resourceId>', identifier)
+    return key
+
+
+def create_resource(resource_template, customer, identifier, username, status):
+    new_resource = copy.deepcopy(resource_template)
+    new_resource['PK0']['S'] = create_pk0(pk0_template=str(
+        new_resource['PK0']['S']), customer=customer, username=username)
+    new_resource['PK1']['S'] = create_pk1(pk1_template=str(
+        new_resource['PK1']['S']), customer=customer, status=status)
+    new_resource['PK2']['S'] = create_pk2(pk2_template=str(
+        new_resource['PK2']['S']), customer=customer, identifier=identifier)
+    new_resource['PK3']['S'] = create_resource_key(
+        template=str(new_resource['PK3']['S']), identifier=identifier)
+    new_resource['SK0']['S'] = create_resource_key(
+        template=str(new_resource['SK0']['S']), identifier=identifier)
+    new_resource['SK1']['S'] = create_resource_key(
+        template=str(new_resource['SK1']['S']), identifier=identifier)
+    new_resource['SK2']['S'] = create_resource_key(template=str(
+        new_resource['SK2']['S']), identifier=identifier).replace('<resourceSort>', 'a')
+    new_resource['SK3']['S'] = create_resource_key(
+        template=str(new_resource['SK3']['S']), identifier=identifier)
+    new_resource['type']['S'] = 'Resource'
+    return new_resource
+
 
 def create_registrations():
     print('Creating registrations...')
-    with open('test_registrations.json') as registrations_file:
-        registrations = json.load(registrations_file)
-        for registration in registrations:
-            identifier = str(uuid.uuid4())
-            registration['identifier']['S'] = identifier
-            print('Created registration with identifier: {}'.format(identifier))
-            try:
-                response = dynamodb_client.put_item(TableName=registrations_tablename,
-                                                    Item=registration)
-            except e:
-                print(e)
+
+    customer = find_customer(archiveName=ARCHIVE_NAME)
+    if customer != 'Not found':
+        with open('resource.json') as resource_file:
+            resource_template = json.load(resource_file)
+        with open('test_registrations.json') as registrations_file:
+            registrations = json.load(registrations_file)
+            for registration in registrations:
+                identifier = str(uuid.uuid4())
+                resource = create_resource(
+                    resource_template=resource_template,
+                    customer=customer,
+                    identifier=identifier,
+                    username=USER_NAME,
+                    status='PUBLISHED'
+                )
+                registration['identifier']['S'] = identifier
+                resource['data']['M'] = registration
+
+                print('Created registration with identifier: {}'.format(identifier))
+                try:
+                    response = dynamodb_client.put_item(TableName=RESOURCE_TABLE_NAME,
+                                                        Item=resource)
+                except e:
+                    print(e)
+    else:
+        print('Customer not found')
+
 
 def run():
     delete_registrations()
     create_registrations()
+
 
 if __name__ == '__main__':
     run()
