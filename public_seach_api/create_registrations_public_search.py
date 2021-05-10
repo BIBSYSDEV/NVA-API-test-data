26a91a476fa4d0cca1f10038be25a693d6ad5e78
Eirik Nilsen <eirik.nilsen@unit.no>
2021-05-10 13:32:05 +0200

    Test data for search - indexed fields

--- public_seach_api/create_registrations_public_search.py
+++ public_seach_api/create_registrations_public_search.py
@@ -9,11 +9,8 @@ USER_POOL_ID = ssm.get_parameter(Name='/test/AWS_USER_POOL_ID',
                                  WithDecryption=False)['Parameter']['Value']
 CLIENT_ID = ssm.get_parameter(Name='/test/AWS_USER_POOL_WEB_CLIENT_ID',
                               WithDecryption=False)['Parameter']['Value']
-# USER_NAME = 'api-test-user@test.no'
 USER_NAME = 'api-test-user@test.no'
 API_ENDPOINT = 'https://api.dev.nva.aws.unit.no/publication'
-print(USER_POOL_ID)
-print(CLIENT_ID)
 
 def login(username):
     client = boto3.client('cognito-idp')
@@ -24,7 +21,6 @@ def login(username):
         Username=username,
         Permanent=True,
     )
-    print(response)
     response = client.admin_initiate_auth(
         UserPoolId=USER_POOL_ID,
         ClientId=CLIENT_ID,
@@ -33,7 +29,6 @@ def login(username):
             'USERNAME': username,
             'PASSWORD': password
         })
-    print(response)
     return response['AuthenticationResult']['IdToken']
 
 def create_registration(registration):
@@ -47,7 +42,6 @@ def create_registration(registration):
 
 def create_registrations(bearer_token):
     print('creating registrations...')
-    print(bearer_token)
     with open('registration_titles.json') as registrations_file:
         registrations = json.load(registrations_file)
         for registration in registrations:
@@ -56,9 +50,10 @@ def create_registrations(bearer_token):
                 'Authorization': 'Bearer {}'.format(bearer_token),
                 'Accept': 'application/json'
             }
-            print(headers)
-            r = requests.post(url = API_ENDPOINT, json = new_registration, headers=headers)
-            print(r.__dict__)
+            createResponse = requests.post(url = API_ENDPOINT, json = new_registration, headers = headers)
+            identifier = createResponse.json()['identifier']
+            publish_url = '{}/{}/publish'.format(API_ENDPOINT, identifier)
+            requests.put(url = publish_url, headers = headers)
 
 def run():
     bearer_token = login(USER_NAME)
